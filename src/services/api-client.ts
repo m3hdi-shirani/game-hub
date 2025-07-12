a0becde471ac8d13670a41593a4cd44ddcc128af
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-12 12:51:37 +0330

    Refactor API client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,14 +1,28 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "d617c273378246f7b621ba54d42c679c",
   },
 });
+
+class ApiClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then((res) => res.data);
+  }
+}
+
+
+export default ApiClient;
